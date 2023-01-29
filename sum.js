de342b1c26bcca62ae56ac63e4159fea0f346bc9
JP Niyitanga <jpniyitanga@gmail.com>
2023-01-29 03:37:00 -0500

    Solution using for loop

--- sum.js
+++ sum.js
@@ -1,23 +1,29 @@
 
-/*
+
 // use slice method to remove the first two default arguments
 const args = process.argv.slice(2);
+
 // Initialize a variable sum
 let sum = 0;
-//Loop through the arguments, convert them to numbers and add them together
+
+// //Loop through the arguments, convert them to numbers and add them together
 for (let i = 0; i < args.length; i++) {
-  sum = sum + Number(args[i]);  
+  if (Number(args[i])) {
+    sum = sum + Number(args[i]);
+  }
 }
 
-console.log(sum)
-*/
-
-const args = process.argv.slice(2);
-//let total = 0;
-args.reduce((total, arg)=>{
-  return sum = Number(total) + Number(arg);
-});
 console.log(sum);
+
+
+
+
+// const args = process.argv.slice(2);
+// let total = 0;
+// args.reduce((total, arg)=>{
+//   return total = Number(total) + Number(arg);
+// });
+// console.log(total);
 
 
 
